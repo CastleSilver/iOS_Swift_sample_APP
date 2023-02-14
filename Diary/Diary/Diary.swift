38b6527d9b853427d39680bafcb55d1edc062e32
CastleSilver <soulll012@naver.com>
2023-02-14 13:36:45 +0900

    일기장 앱 생성

---
+++ Diary/Diary/Diary.swift
@@ -0,0 +1,17 @@
+//
+//  Diary.swift
+//  Diary
+//
+//  Created by tmoney on 2023/02/05.
+//
+
+import Foundation
+
+
+struct Diary {
+    var uuidString: String
+    var title: String
+    var contents: String
+    var date: Date
+    var isStar: Bool
+}
