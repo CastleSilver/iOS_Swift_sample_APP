bfd730d6500d9fb09d92c99d57956537dd21a438
CastleSilver <soulll012@naver.com>
2023-02-14 12:15:59 +0900

    AutoLayout 실습

---
+++ AutoLayoutExample/AutoLayoutExample/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  AutoLayoutExample
+//
+//  Created by tmoney on 2023/01/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var colorView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func tapChangeColorButton(_ sender: UIButton) {
+        self.colorView.backgroundColor = UIColor.blue
+        print("색상 변경 버튼이 클릭되었음")
+    }
+    
+}
+
