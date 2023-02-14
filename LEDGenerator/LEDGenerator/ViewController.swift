08c871929f0e3556484cd1e049a9d3e91363eaf1
CastleSilver <soulll012@naver.com>
2023-02-14 12:18:08 +0900

    LED 전광판 앱 생성

---
+++ LEDGenerator/LEDGenerator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LEDGenerator
+//
+//  Created by tmoney on 2023/01/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
