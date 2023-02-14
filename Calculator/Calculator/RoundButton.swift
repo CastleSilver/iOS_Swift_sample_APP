f955519b8bb9638902c1cfdbfb3a8f3c6c37998f
CastleSilver <soulll012@naver.com>
2023-02-14 12:18:31 +0900

    계산기 앱 생성

---
+++ Calculator/Calculator/RoundButton.swift
@@ -0,0 +1,19 @@
+//
+//  RoundButton.swift
+//  Calculator
+//
+//  Created by tmoney on 2023/01/31.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundButton: UIButton {
+    @IBInspectable var isRound: Bool = false {
+        didSet {
+            if isRound {
+                self.layer.cornerRadius = self.frame.height / 2
+            }
+        }
+    }
+}
