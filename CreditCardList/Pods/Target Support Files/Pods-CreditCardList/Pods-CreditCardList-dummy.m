5c9b66397f31207d753ef990466d515ea24b26aa
CastleSilver <soulll012@naver.com>
2023-11-20 22:16:50 +0900

    기존 프로젝트 삭제

--- CreditCardList/Pods/Target Support Files/Pods-CreditCardList/Pods-CreditCardList-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CreditCardList : NSObject
-@end
-@implementation PodsDummy_Pods_CreditCardList
-@end
--- Notice/Pods/Target Support Files/Pods-Notice/Pods-Notice-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Notice : NSObject
-@end
-@implementation PodsDummy_Pods_Notice
-@end
