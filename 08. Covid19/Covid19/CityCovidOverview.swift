9f8f6b6d3c52b4c14e581040c7288412a2e755d3
CastleSilver <soulll012@naver.com>
2023-11-23 00:17:12 +0900

    코로나 현황파악 앱

---
+++ 08. Covid19/Covid19/CityCovidOverview.swift
@@ -0,0 +1,40 @@
+//
+//  CityCovidOverview.swift
+//  Covid19
+//
+//  Created by 허성은 on 2023/11/22.
+//
+
+import Foundation
+
+struct CityCovidOverView: Codable {
+    let korea: CovidOverview
+    let seoul: CovidOverview
+    let busan: CovidOverview
+    let daegu: CovidOverview
+    let incheon: CovidOverview
+    let gwangju: CovidOverview
+    let daejeon: CovidOverview
+    let ulsan: CovidOverview
+    let sejong: CovidOverview
+    let gyeonggi: CovidOverview
+    let gangwon: CovidOverview
+    let chungbuk: CovidOverview
+    let chungnam: CovidOverview
+    let jeonbuk: CovidOverview
+    let jeonnam: CovidOverview
+    let gyeongbuk: CovidOverview
+    let gyeongnam: CovidOverview
+    let jeju: CovidOverview
+}
+
+struct CovidOverview: Codable {
+    let countryName: String
+    let newCase: String
+    let totalCase: String
+    let recovered: String
+    let death: String
+    let percentage: String
+    let newCcase: String
+    let newFcase: String
+}
